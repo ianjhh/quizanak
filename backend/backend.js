2ae84bc436e3aa5b94c438a0df72bf2abc1609dd
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-11-18 19:28:16 +0700

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -101,7 +101,7 @@ app.post('/api/register', async (req, res) => {
       res.status(200).json(data);
     }
     catch(e){
-      res.status(400).send(e)
+      res.status(400).send('Error!')
     }
 })
 
