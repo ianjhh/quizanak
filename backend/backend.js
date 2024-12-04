1d1404da65a14a2c53482a1bfc4f47b2f7cb8c9b
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-04 18:59:11 +0700

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -146,7 +146,7 @@ app.post('/api/login', async (req, res) => {
 app.post('/api/register', async (req, res) => {
     try{
           let data = req.body;
-          let verificationCode = randomInt(1000_000).toString().padStart(6, '0');
+          let verificationCode = CryptoJS.randomInt(1000_000).toString().padStart(6, '0');
             
           const mailOptions = {
             from: "ianjhh.102@gmail.com",
