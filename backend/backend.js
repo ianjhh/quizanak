50743c8145be4ff2189674f6d3ada88163dd98be
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-05 17:24:08 +0700

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -166,6 +166,7 @@ app.post('/api/login', async (req, res) => {
           jwt.sign({username: req.body.username}, 'privatekey', { expiresIn: '1h' },(err, token) => {
             if(err) { 
                 res.status.send('Error!')
+                console.log(err)
             }
           
           res.status(200).cookie('jwt', token).json({verified: result.verified});
