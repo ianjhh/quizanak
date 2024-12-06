53760bfd73fe9e5cbf313727edd0bb1e9729f319
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-06 16:21:19 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -22,16 +22,16 @@ function Home(props){
         .then(function (response) {
             /* ONLY RUNS IF SUCCESS, NOT EVEN WHEN CODE 404 */
             if (response.data.verified === true){
-                navigate('/')
+                setIsLoggedIn(true)
             }
             else{
                 navigate('/verify')
             }
         })
         .catch(function (error) {
-            console.log('Error!')
+            console.log('Not Logged In!')
         });
-    }
+}
 
    const handleLogin = () =>{
         axios.post('/api/login', {
