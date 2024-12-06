154c0c3ec1bd1ae64b52d27d3ff6f76fc5c1fdde
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-06 16:12:00 +0700

    Update AboutUs.js

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -13,14 +13,14 @@ function AboutUs(){
         .then(function (response) {
             /* ONLY RUNS IF SUCCESS, NOT EVEN WHEN CODE 404 */
             if (response.data.verified === true){
-                navigate('/')
+                setIsLoggedIn(true)
             }
             else{
                 navigate('/verify')
             }
         })
         .catch(function (error) {
-            console.log('Error!')
+            console.log('Not Logged In!')
         });
     }
 
