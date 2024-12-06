404e867583e8360b86c5ab6720ceb8c2e016b7bf
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-06 17:09:20 +0700

    Update Sitemap.js

--- src/Sitemap.js
+++ src/Sitemap.js
@@ -64,7 +64,7 @@ function Sitemap(){
             }
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('error');
         });
     }
 
@@ -77,7 +77,7 @@ function Sitemap(){
             }
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('error');
         });
     }
 
@@ -90,7 +90,7 @@ function Sitemap(){
             }
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('error');
         });
     }
 
@@ -103,7 +103,7 @@ function Sitemap(){
             }
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('error');
         });
     }
 
@@ -116,7 +116,7 @@ function Sitemap(){
             }
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('error');
         });
     }
 
