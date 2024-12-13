0054bd9618703b05ca894d7682b839d2cf0026c7
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-13 17:56:13 +0700

    Update HistoryFacts.js

--- src/HistoryFacts.js
+++ src/HistoryFacts.js
@@ -9,13 +9,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 function HistoryFacts(props){
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [show, setShow] = useState(false);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
     const [historyFacts, setHistoryFacts] = useState([]);
-    const [errMsg, setErrMsg] = useState("");
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
     const navigate = useNavigate();
 
    const verifyToken = () =>{
@@ -34,25 +28,6 @@ function HistoryFacts(props){
         });
 }
 
-    const handleLogin = () =>{
-        axios.post('/api/login', {
-            username: username,
-            password: password
-        })
-        .then(function (response) {
-            /* ONLY RUNS IF SUCCESS, NOT EVEN WHEN CODE 404 */
-            if(response.data.verified === true){
-                navigate('/')
-            }
-            else{
-                navigate('/verify')
-            }
-        })
-        .catch(function (error) {
-            setErrMsg(error.response.data);
-        });
-    }
-
     const fetchRandomFacts = () =>{
         axios.get('/api/fetchRandomFacts')
         .then(function (response) {
@@ -70,30 +45,6 @@ function HistoryFacts(props){
 
     return(
          <>
-            <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Login</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control type="text" onChange={(e)=>{setUsername(e.target.value)}} value={username} />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Kata Sandi</Form.Label>
-                                <Form.Control type="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} />
-                            </Form.Group>
-                            <Form.Text className="text-danger">{errMsg}</Form.Text>
-                            <Button variant="primary" type="button" onClick={handleLogin}>
-                                Masuk
-                            </Button>
-                </Form>
-                <p className='mt-3'>Belum daftar? <Link to='/register' className='text-decoration-none'>Buat akun baru</Link></p>
-            </Modal.Body>
-            </Modal>
-
             {isLoggedIn? <LoggedInNav /> : <Navapp />}
             <div className='bg-warning'>
                 <br />
@@ -103,13 +54,14 @@ function HistoryFacts(props){
                 <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
                     {historyFacts.map((item, idx) => (
                             <Col key={idx} className='fact-card'>
-                            <Card>
+                            <Link to={`/fakta-aneh/${item.link_name}`} className='text-decoration-none'>
+                            <Card className='link-card'>
                                 <Card.Img variant="top" src={require(`./${item.image}.jpg`)} width={200} height={200} />
                                 <Card.Body className='bg-black text-white'>
                                 <Card.Title>{item.title}</Card.Title>
-                                <Button variant="primary" onClick={()=>{if (isLoggedIn){navigate(`/fakta-aneh/${item.link_name}`)} else{handleShow()}}}>Mulai!</Button>
                                 </Card.Body>
                             </Card>
+                            </Link>
                             </Col>
                     ))}
                 </Row><br/><br/>
