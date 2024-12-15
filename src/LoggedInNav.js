342be3c35d767040aa6f0f9fa99dbfc45a400bff
ianjhh <126269866+ianjhh@users.noreply.github.com>
2024-12-15 16:39:18 +0700

    Update LoggedInNav.js

--- src/LoggedInNav.js
+++ src/LoggedInNav.js
@@ -33,7 +33,7 @@ function LoggedInNav(props){
     return (
         <Navbar className="bg-black">
             <Container>
-                <Navbar.Brand><Link to='/' className='text-decoration-none text-info navbar-title'>KuisAnak</Link></Navbar.Brand>
+                <Navbar.Brand><Link to='/' className='text-decoration-none text-white navbar-title'>KuisAnak</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
