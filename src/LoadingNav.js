cc69910b1cfba3813d775da2782dadea3aea12e0
ianjhh <126269866+ianjhh@users.noreply.github.com>
2025-01-02 17:46:47 +0700

    Update LoadingNav.js

--- src/LoadingNav.js
+++ src/LoadingNav.js
@@ -4,14 +4,14 @@ import Nav from 'react-bootstrap/Nav';
 import { Button, NavDropdown } from 'react-bootstrap';
 import './Navapp.css';
 import Placeholder from 'react-bootstrap/Placeholder';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function LoadingNav(){
   return(
     <>
         <Navbar className="bg-black">
             <Container>
-                <Navbar.Brand className='text-white navbar-title fs-5'>KuisAnak</Navbar.Brand>
+                <Navbar.Brand className='text-white navbar-title fs-5'><Link>KuisAnak</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
