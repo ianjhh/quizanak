318456922d09acc7923e186a1cbac437cae030a1
ianjhh <126269866+ianjhh@users.noreply.github.com>
2025-01-02 18:01:42 +0700

    Update LoadingNav.js

--- src/LoadingNav.js
+++ src/LoadingNav.js
@@ -22,7 +22,7 @@ function LoadingNav(){
                         <NavDropdown.Item className='text-black'>Angkasa</NavDropdown.Item>
                         <NavDropdown.Item className='text-black'>Sejarah</NavDropdown.Item>
                     </NavDropdown>
-                    <NavLink><PlaceholderButton variant="secondary" xs={12} animation="glow" /></NavLink>
+                    <PlaceholderButton variant="secondary" xs={7} animation="glow" />
                 </Nav>
                 </Navbar.Collapse>
             </Container>
