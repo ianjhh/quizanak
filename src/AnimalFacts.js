b88306837f86e8d8f654ffaa1adb3d5ee015b529
ianjhh <126269866+ianjhh@users.noreply.github.com>
2025-01-02 19:01:18 +0700

    Update AnimalFacts.js

--- src/AnimalFacts.js
+++ src/AnimalFacts.js
@@ -25,6 +25,7 @@ function AnimalFacts(props){
             }
         })
         .catch(function (error) {
+            setIsLoggedIn(false)
             console.log(error.response.status)
         });
 }
